27c01068dd5654abbf9c20a028bdbfe10bef480d
zainabsohaib <60998648+zainabsohaib@users.noreply.github.com>
2020-05-25 09:49:45 -0700

    Add files via upload

---
+++ encryption/encryption/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace encryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string hash="DM-PROJECT";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(textBox1.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] Keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripdes = new TripleDESCryptoServiceProvider() { Key = Keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = tripdes.CreateEncryptor();
+                    byte[] results = transform.TransformFinalBlock(data, 0, data.Length);
+                    textBox2.Text = Convert.ToBase64String(results, 0, results.Length);
+                   
+
+                }
+            }
+        }
+
+ 
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+              
+            }
+            
+
+        }
+
+
+    }
